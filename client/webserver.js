bb46678348cfb99162548fbed4b6630daa6450e4
MayconVM <mayconvm@gmail.com>
2018-06-04 23:00:58 -0300

    Adicionado https.

--- client/webserver.js
+++ client/webserver.js
@@ -1,25 +1,33 @@
 const socket = require('socket.io');
-const http = require('http')
+// const http = require('http')
+const https = require('https')
 const express = require('express');
 const path = require('path');
 const app = express();
+const fs = require('fs');
+
+
+const privateKey = fs.readFileSync('/faculdade/domain.key', 'utf8');
+const certificate = fs.readFileSync('/faculdade/domain.crt', 'utf8');
+
+const credentials = {key: privateKey, cert: certificate};
 
 const port = 3000
 app.set('port', port);
 
-const server = http.createServer(app);
+const server = https.createServer(credentials, app);
 server.listen(app.get('port'), function () {
   console.log('WebService iniciou. Porta: ' + app.get('port'));
 });
 
 // WebService http://localhost:3000
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/interface/index.html'));
+    res.sendFile(path.join('/faculdade/interface/index.html'));
 });
 
 // script file
 app.get('/interface/app.js', function(req, res) {
-    res.sendFile(path.join(__dirname + '/interface/app.js'));
+    res.sendFile(path.join('/faculdade/interface/app.js'));
 });
 
 // WebSocket
